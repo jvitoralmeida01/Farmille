33f7c29f2a9eb1403c3003377e4c4dbf1e23f20a
Joao Almeida <jvitor@10-48-48-143.0200.ufpe.br.iosda.org>
2023-03-30 11:45:27 -0300

    first commit

---
+++ Farmille/FarmilleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FarmilleApp.swift
+//  Farmille
+//
+//  Created by Joao Almeida on 30/03/23.
+//
+
+import SwiftUI
+
+@main
+struct FarmilleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
